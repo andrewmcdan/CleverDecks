b2ac75c5d9e3279c757cdc5df8d7e77ce5f810b3
Andrew <andrewmcdan@gmail.com>
2024-02-10 20:33:07 -0500

    Basic web server stuff

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-console.log('Hello, world!');
-
 const express = require('express');
 const app = express();
 const port = 3000;
 
+
+
+//////////////////////////////////////////////////////
+// Server endpoints
+//////////////////////////////////////////////////////
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    // forward to /web/
+    res.redirect('/web/');
 });
+
+app.use('/web/', express.static('web', {
+    extensions: ['html', 'htm', 'css', 'js', 'png', 'jpg', 'jpeg', 'gif', 'svg', 'ico', 'json', 'xml', 'webmanifest']
+}));
+
+// 404
+app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname + '/web/404.html');
+});
+
+
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`App running. Point your web browser to http://localhost:${port}`);
 });
-
-console.log('Point your web browser to http://localhost:3000/ to see the result.');
