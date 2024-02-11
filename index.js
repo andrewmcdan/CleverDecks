039f9e113808b823e32ea7a73435b64232ad1e81
Andrew <andrewmcdan@gmail.com>
2024-02-10 20:12:17 -0500

    bootstrap express server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log('Hello, world!');
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+console.log('Point your web browser to http://localhost:3000/ to see the result.');
