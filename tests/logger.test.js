d3a58f6f6e2bee63324f36d299bf05bea4c2e5ee
Andrew <andrewmcdan@gmail.com>
2024-04-03 13:30:48 -0400

    Update Runner

--- tests/logger.test.js
+++ tests/logger.test.js
@@ -1,55 +1,55 @@
-const Logger = require('../logger');
+const {Logger} = require("../logger");
 
-test('Logger class exists', () => {
+test("Logger class exists", () => {
     expect(Logger).toBeDefined();
 });
 
-test('Logger class has a log method', () => {
+test("Logger class has a log method", () => {
     const logger = new Logger();
     expect(logger.log).toBeDefined();
 });
 
-test('Logger class has a writeOutLogs method', () => {
+test("Logger class has a writeOutLogs method", () => {
     const logger = new Logger();
     expect(logger.writeOutLogs).toBeDefined();
 });
 
-test('Logger class has a finalize method', () => {
+test("Logger class has a finalize method", () => {
     const logger = new Logger();
     expect(logger.finalize).toBeDefined();
 });
 
-test('Logger class has a logs property', () => {
+test("Logger class has a logs property", () => {
     const logger = new Logger();
     expect(logger.logs).toBeDefined();
 });
 
-test('Logger class has a consoleLogging property', () => {
+test("Logger class has a consoleLogging property", () => {
     const logger = new Logger();
     expect(logger.consoleLogging).toBeDefined();
 });
 
-test('Logger class log function logs a message', () => {
+test("Logger class log function logs a message", () => {
     const logger = new Logger(true, 1); // true for console logging, 1 for log level
-    const spy = jest.spyOn(console, 'log');
-    logger.log('test message');
+    const spy = jest.spyOn(console, "log");
+    logger.log("test message");
     expect(spy).toHaveBeenCalled();
     spy.mockRestore();
 });
 
-test('Logger class log function does not log a message if log level is off', () => {
+test("Logger class log function does not log a message if log level is off", () => {
     const logger = new Logger(true, 0); // true for console logging, 0 for log level
-    const spy = jest.spyOn(console, 'log');
-    logger.log('test message');
+    const spy = jest.spyOn(console, "log");
+    logger.log("test message");
     expect(spy).not.toHaveBeenCalled();
     spy.mockRestore();
 });
 
-test('Logger class logs a message if and only if the log level is high enough', () => {
+test("Logger class logs a message if and only if the log level is high enough", () => {
     for(let i = 0; i < 6; i++) {
         const logger = new Logger(true, i); // true for console logging, i for log level
-        const spy = jest.spyOn(console, 'log');
-        logger.log('test message', i);
+        const spy = jest.spyOn(console, "log");
+        logger.log("test message", i);
         if(i > 0) {
             expect(spy).toHaveBeenCalled();
         } else {
